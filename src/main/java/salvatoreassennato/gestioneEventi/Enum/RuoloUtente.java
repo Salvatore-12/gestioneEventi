62d8ebced2191a52cf7659e07e82ca9dd83584b0
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-01-26 11:14:53 +0100

    creazione della struttura base

---
+++ src/main/java/salvatoreassennato/gestioneEventi/Enum/RuoloUtente.java
@@ -0,0 +1,6 @@
+package salvatoreassennato.gestioneEventi.Enum;
+
+public enum RuoloUtente {
+    UTENTE_NORMALE,
+    ORGANIZZATORE_DI_EVENTI
+}
---
+++ src/main/java/salvatoreassennato/gestioneEventi/entities/Utente.java
@@ -0,0 +1,11 @@
+package salvatoreassennato.gestioneEventi.entities;
+
+public class Utente {
+
+    private String id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private RuoloUtente ruoloUtente;
+}
